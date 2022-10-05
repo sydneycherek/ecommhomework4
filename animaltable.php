d1b7488c2ea0cb3602ab5586eda27d04b705ebf8
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 14:35:04 -0500

    Update animaltable.php

--- animaltable.php
+++ animaltable.php
@@ -43,6 +43,20 @@ if ($result->num_rows > 0) {
     <td><?=$row["animaltype"]?></td>
     <td><?=$row["animalgender"]?></td>
     <td><a href="enclosurefilterpage.php?id=<?=$row["enclosure_id"]?>"><?=$row["enclosure_id"]?></a></td>
+  
+    <td>
+      <form method="post" action="animal-edit.php">
+        <input type="hidden" name="iid" value="<?=$row["animal_id"]?>" />
+        <input type="submit" value="Edit" class="btn" />
+      </form>
+    </td>
+    <td>
+      <form method="post" action="animal-delete-save.php">
+        <input type="hidden" name="iid" value="<?=$row["animal_id"]?>" />
+        <input type="submit" value="Delete" class="btn" onclick="confirm('Are you sure?')" />
+      </form>
+    </td>
+  </tr>
   </tr>
 <?php
   }
@@ -51,3 +65,8 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
     
+    <br />
+    <a href="animal-add.php" class="btn btn-primary">Add New</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+     </body>
+</html>
