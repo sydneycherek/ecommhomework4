64d2aec566ec35bca3defcf17134b0d3c2e09b3c
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 14:18:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,6 @@
           <th> Animal Name</th>
        </tr>
       </thead>
-      <tbody>
         <?php
 $servername = "localhost";
 $username = "sydneych_homework3";
@@ -60,32 +59,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM Animal";
-$result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["animal_id"]?></td>
-    <td><?=$row["animalname"]?></td>
-    <td>
-      <form method="post" action="postpage.php">
-        <input type="hidden" name="id" value="<?=$row["animal_id"]?>" />
-        <input type="submit" value="Care / Service Type" />
-      </form>
-    </td>
-  </tr>
-<?php
+
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
-   </tbody>
-   </table>
+   
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
