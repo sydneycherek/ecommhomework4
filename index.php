39e1c49f643b42b05443a3eb5de9bc741b155b5e
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 14:19:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -39,30 +39,6 @@
     </div>
   </div>
 </nav>
-    
-    
-        <?php
-$servername = "localhost";
-$username = "sydneych_homework3";
-$password = "BananaSunday";
-$dbname = "sydneych_homework3";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
