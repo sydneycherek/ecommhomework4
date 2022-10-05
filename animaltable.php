3058f602db2a3ae7700c9780a5ac14801817e56c
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 16:09:48 -0500

    Update animaltable.php

--- animaltable.php
+++ animaltable.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Instructors</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
+    <h1>Animals</h1>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -14,7 +15,6 @@
       <th>Name</th>
       <th>Type</th>
       <th>Gender</th>
-      <th>Enclosure ID</th>
     </tr>
   </thead>
   <tbody>
@@ -24,13 +24,14 @@ $username = "sydneych_homework3";
 $password = "BananaSunday";
 $dbname = "sydneych_homework3";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT animal_id, animalname, animaltype, animalgender, enclosure_id from Animal";
+$sql = "SELECT animal_id, animalname, animaltype, animalgender from Animal";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -42,8 +43,6 @@ if ($result->num_rows > 0) {
     <td><?=$row["animalname"]?></td>
     <td><?=$row["animaltype"]?></td>
     <td><?=$row["animalgender"]?></td>
-    <td><a href="enclosurefilterpage.php?id=<?=$row["enclosure_id"]?>"><?=$row["enclosure_id"]?></a></td>
-  
     <td>
       <form method="post" action="animal-edit.php">
         <input type="hidden" name="iid" value="<?=$row["animal_id"]?>" />
@@ -57,17 +56,17 @@ if ($result->num_rows > 0) {
       </form>
     </td>
   </tr>
-  </tr>
 <?php
   }
 } else {
   echo "0 results";
 }
 $conn->close();
-    </tbody>
+?>
+  </tbody>
     </table>
     <br />
-    <a href="animal-add.php" class="btn btn-primary">Add New</a>
+    <a href="animal-add.php" class="btn btn-primary">Add New Aniaml</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-     </body>
+  </body>
 </html>
