c1542f2b3f06394ca0bbaf19c1039f11787f83dc
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 16:13:58 -0500

    Create animal-add-save.php

---
+++ animal-add-save.php
@@ -0,0 +1,40 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Animal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "sydneych_homework3";
+$password = "BananaSunday";
+$dbname = "sydneych_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$iName = $_POST['aName'];
+
+$sql = "insert into Animal (animalname) value (?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $aName);
+    $stmt->execute();
+?>
+    
+    <h1>Add Animal</h1>
+<div class="alert alert-success" role="alert">
+  New Animal added.
+</div>
+    <a href="animaltable.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
