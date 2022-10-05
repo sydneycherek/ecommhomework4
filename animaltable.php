1f4ba28d356d5ad1e0e8d91475725a74bf2c3dec
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-05 14:37:16 -0500

    Update animaltable.php

--- animaltable.php
+++ animaltable.php
@@ -64,7 +64,8 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 $conn->close();
-    
+    </tbody>
+    </table>
     <br />
     <a href="animal-add.php" class="btn btn-primary">Add New</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
