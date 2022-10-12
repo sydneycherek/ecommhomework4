2eb2b77c85e68ec79674e7c4e82f63bffb974c73
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-12 13:48:41 -0500

    Update animal-add.php

--- animal-add.php
+++ animal-add.php
@@ -14,6 +14,16 @@
     <input type="text" class="form-control" id="animalname" aria-describedby="nameHelp" name="aName">
     <div id="nameHelp" class="form-text">Enter the Animal's name.</div>
   </div>
+  <div class="mb-3">
+    <label for="animaltype" class="form-label">Type</label>
+    <input type="text" class="form-control" id="animaltype" aria-describedby="typeHelp" name="aType">
+    <div id="typeHelp" class="form-text">Enter the Type of Animal.</div>
+  </div>
+  <div class="mb-3">
+    <label for="animalgender" class="form-label">Gender</label>
+    <input type="text" class="form-control" id="animalgender" aria-describedby="genderHelp" name="aGender">
+    <div id="genderHelp" class="form-text">Enter the Gender of the Animal.</div>
+  </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
