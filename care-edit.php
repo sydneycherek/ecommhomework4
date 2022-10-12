478026f5721148e7bc634f5bf9be7dfcb4d845d2
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-12 13:43:36 -0500

    Update care-edit.php

--- care-edit.php
+++ care-edit.php
@@ -3,53 +3,51 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Care Edit</title>
+    <title>Edit Care</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Care ID</th>
-      <th>Animal ID</th>
-      <th>Employee ID</th>
-      <th>Service Type</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+    <h1>Edit Care</h1>
+<?php
 $servername = "localhost";
 $username = "sydneych_homework3";
 $password = "BananaSunday";
 $dbname = "sydneych_homework3";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Care where=care_id=?";
-$result = $conn->query($sql);
+$sql = "SELECT * from Care where care_id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_POST['iid']);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["care_id"]?></td>
-    <td><?=$row["animal_id"]?></td>
-    <td><?=$row["employee_id"]?></td>
-    <td><?=$row["servicetype"]?></td>
-    <td>
-      <form method="post" action="care-edit.php">
-        <input type="hidden" name="id" value="<?=$row["care_id"]?>">
-        <input type="submit" value="Edit">
-    </td>
-  </tr>
+<form method="post" action="care-edit-save.php">
+  <div class="mb-3">
+    <label for="animalname" class="form-label">Name</label>
+    <input type="text" class="form-control" id="animalname" aria-describedby="nameHelp" name="aName" value="<?=$row['animalname']?>">
+    <div id="nameHelp" class="form-text">Enter the Animal's name.</div>
+  </div>
+  <input type="hidden" name="iid" value="<?=$row['animal_id']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 <?php
   }
 } else {
   echo "0 results";
 }
 $conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
