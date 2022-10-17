362626da454303055afd7a94daba539f5c3504fa
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-17 14:30:41 -0500

    Update enclosure-add-save.php

--- enclosure-add-save.php
+++ enclosure-add-save.php
@@ -3,17 +3,37 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Add Enclosure</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
     
-   
-    <h1> Add Enclosure </h1>
- <div class="alert alert-success" role="alert">
-  Your Enclosure has been added!
+<?php
+$servername = "localhost";
+$username = "sydneych_homework3";
+$password = "BananaSunday";
+$dbname = "sydneych_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$tName = $_POST['tName'];
+
+$sql = "insert into Enclosure (enclosuretype, enclosuresize) values (?,?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $tName, $_POST['sType']);
+    $stmt->execute();
+?>
+    
+    <h1>Add Enclosure</h1>
+<div class="alert alert-success" role="alert">
+  New Enclosure added.
 </div>
     <a href="enclosuretable.php" class="btn btn-primary">Go back</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>                                           
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
