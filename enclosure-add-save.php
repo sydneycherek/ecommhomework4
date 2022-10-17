c27f18af3d86813991dde8d0454bae839f7e4786
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-10-17 14:26:48 -0500

    Update enclosure-add-save.php

--- enclosure-add-save.php
+++ enclosure-add-save.php
@@ -13,7 +13,7 @@
  <div class="alert alert-success" role="alert">
   Your Enclosure has been added!
 </div>
-    
+    <a href="enclosuretable.php" class="btn btn-primary">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>                                           
   </body>
 </html>
